0e37c1be226aaae737bf21bafa54cc6ad2d4f5fd
Shahir Roswadi <shahirwadi@gmail.com>
2026-06-27 10:00:21 +0800

    day4-exercise1

---
+++ js/student-object.js
@@ -0,0 +1,15 @@
+
+const student = {
+    studentId : "S001",
+    studentName : "Ignacio de Paul",
+    email : "ignacio@example.com",
+    status : "Active",
+};
+
+console.log("=== Student Object ===");
+console.log("{ studentId: '" + student.studentId + "', studentName: '" + student.studentName + "', email: '" + student.email + "', status: '" + student.status + "' }");
+console.log("");
+console.log("Student ID: " + student.studentId);
+console.log("Student Name: " + student.studentName);
+console.log("Email: " + student.email);
+console.log("Status: " + student.status);
